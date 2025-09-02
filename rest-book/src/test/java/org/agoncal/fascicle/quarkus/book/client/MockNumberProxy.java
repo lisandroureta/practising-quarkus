922772a0e4557dea61f53743f89673b0061eff39
lisndroureta <lisandroureta@gmail.com>
2025-09-02 10:14:40 -0300

    Subiendo proyecto Quarkus

---
+++ rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/client/MockNumberProxy.java
@@ -0,0 +1,21 @@
+package org.agoncal.fascicle.quarkus.book.client;
+
+import io.quarkus.test.Mock;
+import org.agoncal.fascicle.quarkus.book.BookResourceTest;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Mock
+@ApplicationScoped
+@RestClient
+public class MockNumberProxy implements NumberProxy {
+
+  @Override
+  public IsbnNumbers generateIsbnNumbers() {
+    IsbnNumbers isbnNumbers = new IsbnNumbers();
+    isbnNumbers.setIsbn13(BookResourceTest.MOCK_ISBN_13);
+    isbnNumbers.setIsbn10(BookResourceTest.MOCK_ISBN_10);
+    return isbnNumbers;
+  }
+}
---
+++ rest-number/src/main/java/org/agoncal/fascicle/quarkus/number/NumberResource.java
@@ -0,0 +1,66 @@
+package org.agoncal.fascicle.quarkus.number;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.time.Instant;
+import com.github.javafaker.Faker;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+
+@Path("/api/numbers/book")
+@Tag(name = "Number Endpoint")
+public class NumberResource {
+
+  private static final Logger LOGGER = Logger.getLogger(NumberResource.class.getName());
+
+  @ConfigProperty(name = "number.separator", defaultValue = "false")
+  boolean separator;
+
+  @ConfigProperty(name = "seconds.sleep", defaultValue = "0")
+  int secondsToSleep = 0;
+
+  @Operation(summary = "Generates book numbers", description = "These book numbers have several formats: ISBN, ASIN and EAN")
+  @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = BookNumbers.class)))
+
+  @Counted(name = "countGenerateBookNumber", description = "Counts how many times the generateBookNumbers method has been invoked")
+  @Timed(name = "timeGenerateBookNumber", description = "Times how long it takes to invoke the generateBookNumbers method", unit = MetricUnits.MILLISECONDS)
+
+  @Timeout(250)
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response generateBookNumbers() throws InterruptedException {
+    LOGGER.info("Waiting for " + secondsToSleep + " seconds");
+    TimeUnit.SECONDS.sleep(secondsToSleep);
+    LOGGER.info("Generating book numbers");
+    Faker faker = new Faker();
+    BookNumbers bookNumbers = new BookNumbers();
+    bookNumbers.setIsbn10(faker.code().isbn10(separator));
+    bookNumbers.setIsbn13(faker.code().isbn13(separator));
+    bookNumbers.setAsin(faker.code().asin());
+    bookNumbers.setEan8(faker.code().ean8());
+    bookNumbers.setEan13(faker.code().ean13());
+    bookNumbers.setGenerationDate(Instant.now());
+    return Response.ok(bookNumbers).build();
+  }
+
+  @GET
+  @Path("/ping")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String ping() {
+    return "ping";
+  }
+}
